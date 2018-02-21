87f3b3106ce610cfed5c9ed718bca70821c6860c
Maram Kamil <Meme@Marams-MacBook-Pro.local>
2018-02-21 09:16:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/meme/eduhr/newUserQ.java
@@ -0,0 +1,14 @@
+package com.example.meme.eduhr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newUserQ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user_q);
+    }
+
+}
